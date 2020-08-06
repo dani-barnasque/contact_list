e52a20b6b42be63826506fe66495a0caa5c090e4
dani-barnasque <daniel.barnasque@kobe.io>
2020-08-06 12:04:58 -0300

    Created functions deleteContact, updateContact, getAllContacts and Close

--- lib/helpers/contact_helpers.dart
+++ lib/helpers/contact_helpers.dart
@@ -55,6 +55,31 @@ class ContactHelper {
       return null;
     }
   }
+
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: '$idColumn = ?', whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: '$idColumn = ?', whereArgs: [contact.id]);
+  }
+
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;
+    List listmap = await dbContact.rawQuery('SELECT * FROM $contactTable');
+    List<Contact> listContact = List();
+    for(Map m in listmap){
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+
+  Future close() async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
 } 
 
 
