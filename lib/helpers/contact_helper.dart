e422870e7425d3095bb48960b2d383df472f1328
dani-barnasque <daniel.barnasque@kobe.io>
2020-08-08 16:01:34 -0300

    Fixes on initDb, Contact Class and New home_page layout

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -29,27 +29,31 @@ class ContactHelper {
 
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath.'contacts.db');
+    final path = join(databasesPath, 'contactsnew.db');
 
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
-      await db.execute(
-        'CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)'
-      );
-    },); 
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int newerVersion) async {
+        await db.execute(
+            'CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)');
+      },
+    );
   }
-  Future<Contact> saveContact (Contact contact) async {
-  Database dbContact = await db;
-  contact.id = await dbContact.insert(contactTable, contact.toMap());
-  return contact;
-}
 
-  Future<Contact> getContact (int id) async {
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
     Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,
-    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
-    where: '$idColumn = ?',
-    whereArgs: [id]);
-    if (maps.length > 0){
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: '$idColumn = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
       return Contact.fromMap(maps.first);
     } else {
       return null;
@@ -58,34 +62,31 @@ class ContactHelper {
 
   Future<int> deleteContact(int id) async {
     Database dbContact = await db;
-    return await dbContact.delete(contactTable, where: '$idColumn = ?', whereArgs: [id]);
+    return await dbContact
+        .delete(contactTable, where: '$idColumn = ?', whereArgs: [id]);
   }
 
   Future<int> updateContact(Contact contact) async {
     Database dbContact = await db;
-    return await dbContact.update(contactTable, contact.toMap(), where: '$idColumn = ?', whereArgs: [contact.id]);
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: '$idColumn = ?', whereArgs: [contact.id]);
   }
 
   Future<List> getAllContacts() async {
     Database dbContact = await db;
     List listmap = await dbContact.rawQuery('SELECT * FROM $contactTable');
     List<Contact> listContact = List();
-    for(Map m in listmap){
+    for (Map m in listmap) {
       listContact.add(Contact.fromMap(m));
     }
     return listContact;
   }
 
-  Future close() async{
+  Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
-} 
-
-
-
-
-
+}
 
 class Contact {
   int id;
@@ -110,14 +111,13 @@ class Contact {
       imgColumn: img,
     };
     if (id != null) {
-      Map<String, dynamic> map = {
-        map[idColumn]: id,
-      };
-      return map;
+      map[idColumn] = id;
     }
-    @override
-    String toString() {
-      return 'Contact(id: $id, name: $name, phone: $phone, img: $img';
-    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return 'Contact(id: $id, name: $name, phone: $phone, img: $img';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:contact_list/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      home: HomePage(),
+    ),
   );
 }
